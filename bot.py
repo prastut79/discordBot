181011c33e1d20bd1787aabbe77232f3b208fbcd
netsos798 <70134219+netsos798@users.noreply.github.com>
2020-09-20 19:51:29 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
 #https://discordpy.readthedocs.io/en/latest/logging.html
 
+
+from os import environ
+from datetime import datetime 
+from typing import Optional
+import json
+
+
 import discord
 from discord.ext import commands
 from random import choice 
-import os
-from datetime import datetime 
-import csv
-import json
+from discord import Embed, Member
+from discord.ext.commands import command
+from discord.ext import tasks
+
 
 HEX_COLORS=[
     0x4B4CAD, 0xA2D7CC, 0x74AD4B, 0x000000, 0x52fff1, 0xFF51EB, 0xC481A7, 0xffadad, 0xe29578, 0xBD93BD,
@@ -76,6 +83,12 @@ async def on_member_join(member):
     await member.send(choice(ANIME_GIRL_GIFS))         #SEND GIFs
 
 
+
+
+    channel = member.guild.get_channel(757225313943027772)
+    await channel.edit(name=f'All Members: {len(member.guild.members)}')
+
+
     #-----------------------------------------------------------#WELCOME MESSAGE
     #GIVE DJ AND MEMBER ROLE ON JOIN
     role_member = discord.utils.get(member.guild.roles, name='Member')
@@ -140,6 +153,15 @@ async def on_member_remove(member):
     channel = member.guild.get_channel(756452827395784775)
 
     await channel.send(embed=embed)
+
+
+
+    #TOTAL MEMBERS COUNT
+    try:
+        channel = member.guild.get_channel(757225313943027772)
+        await channel.edit(name=f'All Members: {len(member.guild.members)}')
+    except:
+        pass
 
     if str(member.guild)=='Netsos':
         #OPENING JSON FILE
@@ -209,9 +231,73 @@ async def clear_error(ctx, error):
  #========================================================   
 
 
+#USER INFO
+
+@bot.command(name="userinfo", aliases=["memberinfo", "ui", "info"])
+async def user_info(ctx, target: Optional[Member]):
+    target = target or ctx.author
+
+    embed = Embed(title="User Information",
+                    colour=target.colour,
+                    timestamp=datetime.utcnow())
+
+    embed.set_thumbnail(url=target.avatar_url)
+
+    fields = [("Name", str(target), True),
+                ("ID", target.id, True),
+                ("Bot?", target.bot, True),
+                ("Top role", target.top_role.mention, True),
+                ("Status", str(target.status).title(), True),
+                ("Activity", f"{str(target.activity.type).split('.')[-1].title() if target.activity else 'N/A'} {target.activity.name if target.activity else ''}", True),
+                ("Created at", target.created_at.strftime("%b %d, %Y "), True),
+                ("Joined at", target.joined_at.strftime("%b %d, %Y "), True)]
+                # ("Boosted", bool(target.premium_since), True)]
+
+    for name, value, inline in fields:
+        embed.add_field(name=name, value=value, inline=inline)
+
+    await ctx.send(embed=embed)
+
+#SERVER INFO
+
+@bot.command(name="serverinfo", aliases=['serverinformation', "si"])
+async def server_info(ctx):
+    embed = Embed(title="Server information",
+                    colour=ctx.guild.owner.colour,
+                    timestamp=datetime.utcnow())
+
+    embed.set_thumbnail(url=ctx.guild.icon_url)
+
+    statuses = [len(list(filter(lambda m: str(m.status) == "online", ctx.guild.members))),
+                len(list(filter(lambda m: str(m.status) == "idle", ctx.guild.members))),
+                len(list(filter(lambda m: str(m.status) == "dnd", ctx.guild.members))),
+                len(list(filter(lambda m: str(m.status) == "offline", ctx.guild.members)))]
+
+    fields = [("ID", ctx.guild.id, True),
+                ("Owner", ctx.guild.owner, True),
+                ("Region", str(ctx.guild.region).title(), True),
+                ("Created on", ctx.guild.created_at.strftime("%b %d, %Y %H:%M:%S"), True),
+                ("Members", len(ctx.guild.members), True),
+                ("Humans", len(list(filter(lambda m: not m.bot, ctx.guild.members))), True),
+                ("Bots", len(list(filter(lambda m: m.bot, ctx.guild.members))), True),
+                ("Banned members", len(await ctx.guild.bans()), True),
+                ("Statuses", f"🟢 {statuses[0]} 🟠 {statuses[1]} 🔴 {statuses[2]} ⚪ {statuses[3]}", True),
+                ("Text channels", len(ctx.guild.text_channels), True),
+                ("Voice channels", len(ctx.guild.voice_channels), True),
+                ("Categories", len(ctx.guild.categories), True),
+                ("Roles", len(ctx.guild.roles), True),
+                ("Invites", len(await ctx.guild.invites()), True),
+                ("\u200b", "\u200b", True)]
+
+    for name, value, inline in fields:
+        embed.add_field(name=name, value=value, inline=inline)
+
+    await ctx.send(embed=embed)
 
 
-bot.run(os.environ.get('DISCORD_TOKEN'))
+
+
+bot.run(environ.get('DISCORD_TOKEN'))
 
 
     
