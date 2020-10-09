5660f60e7632a8fa8cb1de7fe2044d42796434d0
netsos798 <netsos798@gmail.com>
2020-10-09 18:37:24 +0545

    d

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from typing import Optional
 import csv
 import random
 from time import time
+import smtplib
 
 import discord
 from discord.ext import commands
@@ -30,7 +31,7 @@ HEX_COLORS=[
 SERVER_PREFIX='-' 
 
 bot = commands.Bot(command_prefix=SERVER_PREFIX, case_insensitive=True)
-
+bot.remove_command('help')
 
 
 #bot Start
@@ -90,7 +91,7 @@ async def alistening(ctx,*name):
 async def on_member_join(member):
     #WELCOME MESSAGE------------------------------------------------------------
     embed=discord.Embed(color=random.choice(HEX_COLORS), 
-                        description=f'\n\n{member.mention}, Welcome to **{member.guild}**.:tada:\n\nMember **#{len(list(member.guild.members))}**'
+                        description=f'{member.mention}, Welcome to **{member.guild}**.:tada:\n\nMember **#{len(list(member.guild.members))}**'
         )
     embed.set_thumbnail(url=f'{member.avatar_url}')
     embed.set_author(name=f'{member.name}#{member.discriminator}', icon_url=f'{member.avatar_url}')
@@ -594,7 +595,7 @@ async def listemoji(ctx, extra='list'):
                 k=0
                 animated=''
                 for i in range(int(len(animated_list)/5)+1):
-                    animated+= ' '.join(animated_list[k:k+5])+'n'
+                    animated+= ' '.join(animated_list[k:k+5])+'\n'
                     k+=5
                 animated= animated.strip('\n')
                 
@@ -630,6 +631,112 @@ async def massreactemoji(ctx, channel, message, *args):
 
     await ctx.message.add_reaction('☑')
 # ========================================================
+
+
+#send mail
+@bot.command()
+async def sendmail(ctx):
+    if ctx.author.id == BOT_OWNER_ID:
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            email = environ.get('GMAIL_EMAIL')
+            password = environ.get('KHAI_K_HO')
+
+
+
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            try:
+                #target mail address
+                await ctx.send('Enter Target Email-Address')
+                temp = await bot.wait_for('message', check=check, timeout=30)
+                await temp.delete()
+                target = temp.content
+
+                if '.' not in target or '@' not in target:
+                    await ctx.send('Invalid Email Address')
+                    return
+
+                #mail subjct
+                await ctx.send('Enter mail subject')
+                temp = await bot.wait_for('message', check=check, timeout=50)
+                await temp.delete()
+                subject = temp.content
+
+
+                #mail content
+                await ctx.send('Enter Content')
+                temp = await bot.wait_for('message', check=check, timeout=60)
+                await temp.delete()
+                content = temp.content
+
+ 
+        
+                
+                
+                #confirmation message
+                embed = Embed(
+                    title='Mail Log',
+                    color=random.choice(HEX_COLORS)
+                )
+                embed.add_field(name='Target', value=target, inline=False)
+                embed.add_field(name='Subject', value=subject, inline=False)
+                embed.add_field(name='Content', value=content, inline=False)
+
+                embed = await ctx.send(content='Confirm?',embed=embed)
+                await embed.add_reaction('☑')
+                await embed.add_reaction('❌')
+
+                def check_emj(reaction, user):
+                    return user == ctx.author and (str(reaction.emoji) == '☑' or str(reaction.emoji) == '❌')
+
+                #confirm mail send
+
+                reaction, user = await bot.wait_for('reaction_add', check=check_emj, timeout=300)
+
+                await embed.delete()
+
+                reaction = str(reaction)
+                if reaction == '☑':
+
+                    # attempt login
+                    try:
+                        status = await ctx.send(content='Logging in...')
+                        smtp.login(email, password)
+                    except:
+                        await status.edit('Couldnot login. Check the credentials and try again.')
+                        return
+
+                    await status.edit(content='Attempting to Send Mail...')
+
+                    msg= f'Subject: {subject}\n\n{content}'
+                    smtp.sendmail(email, target, msg)
+                    
+                    await status.edit(content=f'{ctx.author.mention}\nMail Sent Succssfully!')
+
+                elif reaction == '❌':
+                    print('no')
+                    await ctx.send('Mailing Request Cancelled')
+                
+
+            except asyncio.TimeoutError:
+                await temp.edit(content='Request Timedout')
+
+
+            except smtplib.SMTPRecipientsRefused:
+                await status.edit(content='Invalid Email Address')
+
+
+            except:
+                await ctx.send('Errorrrrr')
+
+            
+
+
 
 
 #print Zer0
@@ -679,22 +786,22 @@ async def LogOut(ctx):
 
 
 # Error Handling
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-        pass
-    elif isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-        pass
-    elif isinstance(error, discord.ext.commands.errors.CommandInvokeError):
-        pass
-    elif isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
-        await ctx.message.add_reaction('⏳')
-    elif isinstance(error, discord.ext.commands.errors.MissingRole):
-        pass
+# @bot.event
+# async def on_command_error(ctx, error):
+#     if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+#         pass
+#     elif isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+#         pass
+#     elif isinstance(error, discord.ext.commands.errors.CommandInvokeError):
+#         pass
+#     elif isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
+#         await ctx.message.add_reaction('⏳')
+#     elif isinstance(error, discord.ext.commands.errors.MissingRole):
+#         pass
 
-DISCORD_TOKEN = environ.get('DISCORD_TOKEN') 
-
-bot.run(DISCORD_TOKEN)
+# DISCORD_TOKEN = environ.get('DISCORD_TOKEN') 
+bot.run('NzU2ODE2NTEzMDM3NzYyNTgx.X2XWTQ.h-3pujN5KbKbqnDsAhtVq7RRHKQ')
+# bot.run(DISCORD_TOKEN)
 
 #========================================================  
 #========================================================  
